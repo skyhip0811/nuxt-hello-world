c90710d09b270f13c360765233eeed7f71daef86
skyhip0811 <mr.quock@gmail.com>
2023-10-22 19:31:08 +0800

    das

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  app: {
-    pageTransition: { name: 'page', mode: 'out-in' }
-  },
+
 })
